68a6530b9e89c27219e8437cbcb7cc5c62012b13
Mallik Begur <mbegur@gmail.com>
2017-09-08 15:39:44 -0700

    add Search class

--- Star_Wars_search/entry.jsx
+++ Star_Wars_search/entry.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export const fetchSearchResult = (text) => {
+const fetchSearchResult = (text) => {
   return fetch(`https://swapi.co/api/people/?search=${text}`)
   .then(function(response) {
     return response.json();
   }).then(console.log);
 };
+
+class Search extends React.Component {
+  constructor(props) {
+    this.state = {
+      searchParams: ""
+    };
+  }
+
+  setSearchParams(e) {
+    this.setState({
+      searchParams: e.target.value
+    });
+  }
+
+  searchResults() {
+    let starWars = fetchSearchResult(this.state.searchParams);
+    let allResults = starWars.results;
+    
+
+
+  }
+
+  render() {
+
+  }
+}
 
 class Root extends React.Component {
   render() {
