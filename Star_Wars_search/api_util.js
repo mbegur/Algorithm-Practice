e11103922263318e51cebf9252b301cb2778480f
Mallik Begur <mbegur@gmail.com>
2017-09-08 15:31:57 -0700

    Add api utilfunction

--- Star_Wars_search/api_util.js
+++ Star_Wars_search/api_util.js
@@ -1,5 +1,6 @@
-export const fetchSearchResult = (
-  fetch(
-    
-  )
-)
+export const fetchSearchResult = (text) => {
+  return fetch(`https://swapi.co/api/people/?search=${text}`)
+  .then(function(response) {
+    return response.json();
+  }).then(console.log);
+};
--- Star_Wars_search/entry.jsx
+++ Star_Wars_search/entry.jsx
@@ -1,2 +1,23 @@
-import React from 'react'
-import React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export const fetchSearchResult = (text) => {
+  return fetch(`https://swapi.co/api/people/?search=${text}`)
+  .then(function(response) {
+    return response.json();
+  }).then(console.log);
+};
+
+class Root extends React.Component {
+  render() {
+    return(
+      <div>
+        <Search/>
+      </div>
+    );
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<Root/>, document.getElementById('main'));
+});
