0aa1d3ea72d7489ff43a06bf562c5ddb4d5ee69c
Mallik Begur <mbegur@gmail.com>
2017-09-13 14:24:27 -0700

    vanilla challenge complete

--- codingchallenge1/vanilla_reddit_challenge/search.js
+++ codingchallenge1/vanilla_reddit_challenge/search.js
@@ -1,2 +1,44 @@
-const searchInput = document.querySelector(".searchbar");
-const searchResults = document.querySelector(".searchresults")
+const searchInput = document.querySelector(".search-input");
+const searchResults = document.querySelector(".search-results");
+const submitButton = document.querySelector(".submit-button");
+const submitForm = document.querySelector(".search-form");
+
+const fetchSearchResults = () => {
+  console.log(searchInput.value);
+  return fetch(`https://www.reddit.com/user/${searchInput.value}/submitted.json`)
+    .then(res => res.json())
+    .then(res2 => parseResults(res2));
+};
+
+const parseResults = (results) => {
+  let array = results.data.children;
+  let sortedArray = array.sort((a, b) => {
+    return b.data.score - a.data.score;
+  });
+  let listElements = sortedArray.map((element, idx) => {
+    return `<li>${element.data.score} ${element.data.title}</li>`;
+  }).join('');
+
+  searchResults.innerHTML = listElements;
+};
+
+submitForm.onsubmit = (e) => {
+  e.preventDefault();
+  fetchSearchResults();
+};
+
+// searchInput.addEventListener('input', fetchSearchResults);
+
+// submitButton.onclick = (e) => {
+//   e.preventDefault();
+//   fetchSearchResults();
+//   // return false;
+// };
+
+// submitButton.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   fetchSearchResults();
+//   // return false;
+//   }
+// );
+// submitButton.addEventListener('mouseup', () => {return false;}));
