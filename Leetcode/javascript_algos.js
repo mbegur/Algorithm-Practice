bd910d9d85bb94e1b9017befdfc0f616be9eb034
Mallik Begur <mbegur@gmail.com>
2017-09-23 23:49:08 -0700

    Add mergesort to javascript algos

--- Leetcode/javascript_algos.js
+++ Leetcode/javascript_algos.js
@@ -49,3 +49,30 @@ Array.prototype.quickSort = function () {
 
   return left.quickSort().concat(pivot, right.quickSort());
 };
+
+function mergeSort(array) {
+  if (array.length <= 1) {
+    return array;
+  } else {
+    const mid = Math.floor(array.length / 2);
+
+    const left = mergeSort(array.slice(0, mid));
+    const right = mergeSort(array.slice(mid));
+
+    return merge(left, right);
+  }
+}
+
+function merge(left, right) {
+
+  const sorted = [];
+  while (left.length > 0 && right.length > 0) {
+
+    if (left[0] <= right[0]) {
+      sorted.push(left.shift());
+    } else if (right[0] < left[0]){
+      sorted.push(right.shift());
+    }
+  }
+  return sorted.concat(left, right);
+}
